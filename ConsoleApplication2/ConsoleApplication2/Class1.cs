a48cdf1a291f3e586a7bd7a6cb258ee8835ab3b7
lnikolickezman <Lazar.Nikolic@comtradesoftware.com>
2017-08-22 14:40:53 +0200

    Probaj branch

--- ConsoleApplication2/ConsoleApplication2/Class1.cs
+++ ConsoleApplication2/ConsoleApplication2/Class1.cs
@@ -39,7 +39,7 @@ namespace ConsoleApplication2
 
         public override string ToString()
         {
-            return "Personalizacija " + Ime + " " + Broj;
+            return "Person: 1. " + Ime + " " + Broj;
         }
 
     }
