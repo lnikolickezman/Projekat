9b3e970bf661b9792b3897deab50770e145cd75e
lnikolickezman <31238718+lnikolickezman@users.noreply.github.com>
2017-08-22 13:54:12 +0200

    Add files via upload
    Upload Project

---
+++ ConsoleApplication2/ConsoleApplication2/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Person
+    {
+        private int broj;
+        private string ime;
+
+        public int Broj
+        {
+            get
+            {
+                return broj;
+            }
+
+            set
+            {
+                broj = value;
+            }
+        }
+
+        public string Ime
+        {
+            get
+            {
+                return ime;
+            }
+
+            set
+            {
+                ime = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Person: " + Ime + " " + Broj;
+        }
+
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Broj = 2;
+            p.Ime = "Pera";
+            Console.WriteLine("Genres: {0}, {1}", p.Broj, p.Ime);
+            Console.WriteLine("{0}", p);
+            Console.ReadKey();
+        }
+    }
+}
